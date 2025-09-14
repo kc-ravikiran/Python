3ae0a2563f8c714f9677ea03459dc1fed031e0f2
Ravikiran K C <146708668+24Ravikirankc@users.noreply.github.com>
2025-09-14 13:39:46 +0530

    Create Basic.py

---
+++ Basics/Basic.py
@@ -0,0 +1,66 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+2 + 3
+5
+5/2
+2.5
+5 // 2
+2
+int x =10, y = 12;
+SyntaxError: invalid syntax
+int x =10, y = 12;
+SyntaxError: invalid syntax
+
+(4 + 5)- 2
+7
+(3+3)*3 -4
+14
+( 3+3) * 3 - 4 *3
+6
+11 % 3
+2
+2 * 2 * 3
+12
+3 ** 5
+243
+10 / 3
+3.3333333333333335
+10 // 3
+3
+10 % 3
+1
+" Kiran"
+' Kiran'
+print(hbhb)
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    print(hbhb)
+NameError: name 'hbhb' is not defined
+print('v m')
+v m
+print (" vjh")
+ vjh
+print(" navin's laptop")
+ navin's laptop
+print('kiran\'s "laptop" ')
+kiran's "laptop" 
+10 * kiran
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    10 * kiran
+NameError: name 'kiran' is not defined
+10 * 'kiran'
+'kirankirankirankirankirankirankirankirankirankiran'
+print('C:/Docs/Kiran')
+C:/Docs/Kiran
+print(' c:Docs/na')
+ c:Docs/na
+print(' C:\Docs\na')
+ C:\Docs
+a
+Print (r ' c:\Docs\na')
+SyntaxError: invalid syntax
+print(r ' c:\docs\na')
+SyntaxError: invalid syntax
+print(r'c:\docs\na')
+c:\docs\na
